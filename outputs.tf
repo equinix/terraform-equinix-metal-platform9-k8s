0c822695e86e0414dfc7d30265cf99dcc85927a2
Cody Hill <cody@packet.com>
2020-08-09 03:33:50 +0000

    Updated Terraform formating, Renamed the Repo, Updated the README.md

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "Master_IPs" {
-    value = packet_device.k8s_masters.*.access_public_ipv4
+  value = packet_device.k8s_masters.*.access_public_ipv4
 }
 
 output "Worker_IPs" {
-    value = packet_device.k8s_workers.*.access_public_ipv4
+  value = packet_device.k8s_workers.*.access_public_ipv4
 }
 
 output "Master_LB_IP" {
-    value = packet_reserved_ip_block.cluster_ip.address
+  value = packet_reserved_ip_block.cluster_ip.address
 }
